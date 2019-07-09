c56c23b9a64c01f57a0377705e2ca135a16784b0
MeganPaffrath <meganpaffrath@gmail.com>
2019-07-08 19:13:27 -0700

    Problem_7

--- src/main/java/com/meganpaffrath/Problem_7.java
+++ src/main/java/com/meganpaffrath/Problem_7.java
@@ -1,31 +1,33 @@
-// Author: Megan Paffrath, meganpaffrath.com
-// Date: July 8, 2019
-// Problem: Reverse Integer
-    // Source: https://leetcode.com/problems/reverse-integer/
-
 /*
-    Given a 32-bit signed integer, reverse digits of an integer.
-
-    Example 1:
-
-    Input: 123
-    Output: 321
-    Example 2:
-
-    Input: -123
-    Output: -321
-    Example 3:
-
-    Input: 120
-    Output: 21
-    Note:
-    Assume we are dealing with an environment which could only store integers
-    within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
-    of this problem, assume that your function returns 0 when the reversed
-    integer overflows.
+    Author: Megan Paffrath, meganpaffrath.com
+    Date: July 8, 2019
+    Problem: Reverse Integer
+    Source: https://leetcode.com/problems/reverse-integer/
 */
 
 package com.meganpaffrath;
+
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ *
+ * Input: 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: 120
+ * Output: 21
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
+ * of this problem, assume that your function returns 0 when the reversed
+ * integer overflows.
+ */
 
 public class Problem_7 {
     public int reverse(int x) {
