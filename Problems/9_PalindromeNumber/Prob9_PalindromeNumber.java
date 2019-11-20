d2694aaf9d4b6df6f6bcdeb13b602b6310142535
MeganPaffrath <meganpaffrath@gmail.com>
2019-11-20 12:48:49 -0800

    split value in half first, did not improve memory, worsened runtime

--- Problems/9_PalindromeNumber/Prob9_PalindromeNumber.java
+++ Problems/9_PalindromeNumber/Prob9_PalindromeNumber.java
@@ -1,9 +1,12 @@
 package com.meganpaffrath;
 /**
  * IMPROVE MEMORY USAGE
+ * do so by only reverting half of the number?
+ *
  *
  * Runtime: 6 ms, faster than 100.00% of Java online submissions for Palindrome Number.
  * Memory Usage: 35.9 MB, less than 5.02% of Java online submissions for Palindrome Number.
+
  */
 
 
@@ -30,7 +33,10 @@ package com.meganpaffrath;
  * Coud you solve it without converting the integer to a string?
  */
 
-// IMPROVED METHOD:
+/**
+ * Runtime: 6 ms, faster than 100.00% of Java online submissions for Palindrome Number.
+ * Memory Usage: 35.9 MB, less than 5.02% of Java online submissions for Palindrome Number.
+ */
 public class Prob9_PalindromeNumber {
     public boolean isPalindrome(int x) {
         if (x < 0)
@@ -49,4 +55,43 @@ public class Prob9_PalindromeNumber {
         return false;
     }
 }
+
+// Splitting x first did not improve time or memory
+/**
+ * Runtime: 8 ms, faster than 40.40% of Java online submissions for Palindrome Number.
+ * Memory Usage: 36.2 MB, less than 5.02% of Java online submissions for Palindrome Number.
+ */
+//public class Prob9_PalindromeNumber {
+//    public boolean isPalindrome(int x) {
+//        if (x < 0)
+//            return false;
+//        int xLength = String.valueOf(x).length();
+//        if (xLength == 1)
+//            return true;
+//        int splitLength = 0;
+//        int leadingVals = x;
+//        int endingVals = 0;
+//        int temp = x;
+//
+//        if (xLength%2 != 0) { // odd
+//            splitLength = (xLength - 1)/2;
+//        } else {
+//            splitLength = xLength / 2;
+//        }
+//
+//        while (splitLength > 0) { // append to ending vals
+//            // Append to ending vals and remove from leading vals
+//            endingVals = endingVals*10 + leadingVals%10;
+//            leadingVals = leadingVals/10;
+//            splitLength--;
+//        }
+//        if (xLength%2 != 0) { // if odd remove another val from leadingVals
+//            leadingVals = leadingVals/10;
+//        }
+//
+//        if (leadingVals == endingVals)
+//            return true;
+//        return false;
+//    }
+//}
 
